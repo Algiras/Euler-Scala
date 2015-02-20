d6d8de5037ec9839a18f7fd026babfea88ebc7ee
Algimantas Krasauskas <krasauskas.algimantas@outlook.com>
2015-02-20 14:33:58 +0200

    10001st prime

---
+++ src/main/scala/Problem7.scala
@@ -0,0 +1,12 @@
+object Problem7 {
+  def main(args: Array[String]) {
+    lazy val primes: Stream[Int] =
+      2 #:: Stream.from(3).filter(i =>
+        primes.takeWhile(j => j * j <= i).forall(k => i % k > 0)
+      )
+
+    val result = primes.take(10001).last
+
+    println(result)
+  }
+}
